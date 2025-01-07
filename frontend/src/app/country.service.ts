2d41fb8bfe906b32cf89d7ec751c7f40d5cf73ad
Shaiful Islam Emon <eshaifulislam@gmail.com>
2025-01-07 11:29:00 +0600

    final commit

---
+++ frontend/src/app/country.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+interface Country {
+  name: { common: string };
+  capital: string[];
+  region: string;
+  population: number;
+  flags: { png: string };
+}
+
+interface SimplifiedCountry {
+  name: string;
+  capital: string;
+  region: string;
+  population: number;
+  flag: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountryService {
+  private apiUrl = 'http://localhost:3000/api/countries';
+
+  constructor(private http: HttpClient) {}
+  getCountries(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map((data) =>
+        data.map((country) => ({
+          name: country.name.common,
+          capital: country.capital?.[0] || 'N/A',
+          region: country.region,
+          population: country.population,
+          flag: country.flags?.png || 'default-flag-url.png',
+        }))
+      )
+    );
+  }
+}
